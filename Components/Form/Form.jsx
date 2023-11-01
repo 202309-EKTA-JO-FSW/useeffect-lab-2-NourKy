338c999488482829971221a4a387ff5b30c94682
Nour kayyali <nourkayyali91@gmail.com>
2023-11-02 00:04:59 +0300

    Completed assignment

--- Components/Form/Form.jsx
+++ Components/Form/Form.jsx
@@ -1,10 +1,26 @@
-export default function Form() {
+import React ,{ useState } from "react";
+export default function Form({onInput}) {
+  const [inputUserName,setInputUserName]=useState('');
+  function handleInput(event)
+  {
+    event.preventDefault();
+    console.log("inputUserNameSub",inputUserName)
+    onInput(inputUserName);
+
+  }
+ function handleInputText(event)
+ {
+  const updatedValue = event.target.value;
+  setInputUserName(updatedValue);
+  console.log("inputUserName", updatedValue);
+ }
   return (
-    <form>
+    <form onSubmit={handleInput}>
       <h3>Search user Github</h3>
       <div className="mb-3">
         <label>User name</label>
-        <input type="text" className="form-control" placeholder="User Name" />
+        <input type="text" className="form-control" placeholder="User Name" onChange={handleInputText} />
+        <button type="submit">enter</button>
       </div>
     </form>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,4 @@
+import React ,{ useState,useEffect } from 'react' 
 import Form from '../Components/Form/Form'
 import UserCard from '../Components/UserCard/UserCard'
 import { Inter } from 'next/font/google'
@@ -6,10 +7,37 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+  const [userName,setUserName]=useState('');
+  const[userData,setUserData]=useState({});
+ // console.log(`userNamevalue`,userName);
+  useEffect(() => {
+    if (userName) {
+      fetch(`https://api.github.com/users/${userName}`)
+        .then((r) => {
+          if (!r.ok) {
+            throw new Error('Network response was not ok');
+          }
+          return r.json();
+        })
+        .then((data) => setUserData(data))
+        .catch((error) => {
+          console.error('Error fetching data:', error);
+          // Handle the error as needed
+        });
+    }
+  }, [userName]);
+  function updateUserName(input)
+  {
+    console.log("input",input)
+    setUserName(input);
+
+  }
+  
+  
   return (
     <div className="card">
-      <Form />
-      <UserCard />
+      <Form onInput={updateUserName} />
+      <UserCard userData={userData}/>
     </div>
   );
 }
